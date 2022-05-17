e735f14055522f2aac842d36f8e37d1c917f845b
Gaurav Sarawgi <gsarawgi@1worldsync.com>
2022-05-17 14:09:41 +0530

    Added new Project AsyncEnumerable in Chapter 13

---
+++ CSharp 9 and .Net 5 Book/Chapter13/AsyncEnumerable/Program.cs
@@ -0,0 +1,27 @@
+﻿using static System.Console;
+// See https://aka.ms/new-console-template for more information
+//WriteLine("Hello, World!");
+
+await MainSync();
+
+static async Task MainSync()
+{
+    await foreach(int numbers in GetNumbers())
+    {
+        WriteLine($"Numbers:{numbers}");
+    }
+}
+
+async static IAsyncEnumerable<int> GetNumbers()
+{
+    var r = new Random();
+
+    await Task.Run(() => Task.Delay(r.Next(1500,3000)) );
+    yield return r.Next(0,1001);
+
+    await Task.Run(() => Task.Delay(r.Next(1500,3000)) );
+    yield return r.Next(0,1001);
+
+    await Task.Run(() => Task.Delay(r.Next(1500,3000)) );
+    yield return r.Next(0,1001);
+}
