8b30a958f4fb22928d97c09d774dec449874c688
sarawgeek <gsarawgi@1worldsync.com>
2022-05-20 16:28:51 +0530

    Added new project in Chapter 2

---
+++ CSharp 9 and .Net 5 Book/Chapter2/Arguments/Program.cs
@@ -0,0 +1,43 @@
+﻿using static System.Console;
+// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+namespace Arguments
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            WriteLine($"There are {args.Length} arguments");
+
+            ForegroundColor = (ConsoleColor)Enum.Parse(
+                enumType: typeof(ConsoleColor),
+                value: args[0],
+                ignoreCase: true
+            );
+
+            BackgroundColor = (ConsoleColor)Enum.Parse(
+                enumType: typeof(ConsoleColor),
+                value: args[1],
+                ignoreCase:true
+            );
+
+            try
+            {
+                CursorSize = int.Parse(args[2]);
+            }
+            catch(PlatformNotSupportedException)
+            {
+                WriteLine("Cursor Size API is not supported in the current platform");
+            }
+
+            
+
+            
+
+/*             foreach(var arg in args)
+            {
+                WriteLine(arg);
+            }  */           
+        }
+    }
+}
--- CSharp 9 and .Net 5 Book/Chapter2/Formatting/Program.cs
+++ CSharp 9 and .Net 5 Book/Chapter2/Formatting/Program.cs
@@ -16,6 +16,27 @@ string formatted = string.Format(
     arg1: numberOFApples*pricePerApple
 );
 
-Console.WriteLine(formatted);
+//Console.WriteLine(formatted);
 
-Console.WriteLine($"{numberOFApples} apples costs {numberOFApples*pricePerApple}");
+//Console.WriteLine($"{numberOFApples} apples costs {numberOFApples*pricePerApple}");
+
+string appleText = "Apple";
+int appleCount = 1234;
+
+string bananaText = "Banana";
+int bananaCount = 56789;
+
+Console.WriteLine(
+    format: "{0,-8} {1,6:N0}",
+    arg0: "Name",
+    arg1: "Count");
+
+Console.WriteLine(
+    format: "{0,-8} {1,6:N0}",
+    arg0: appleText,
+    arg1: appleCount);
+
+    Console.WriteLine(
+    format: "{0,-8} {1,6:N0}",
+    arg0: bananaText,
+    arg1: bananaCount);
