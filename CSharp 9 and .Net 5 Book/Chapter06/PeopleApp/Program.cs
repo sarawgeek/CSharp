cb1054cdf7e202802a7cd37ced7b12688583105f
sarawgeek <gsarawgi@1worldsync.com>
2022-09-12 16:02:50 +0530

    Static and Instance Methods, Operator Overloading

---
+++ CSharp 9 and .Net 5 Book/Chapter06/PeopleApp/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Packt.Shared;
+using static System.Console;
+
+namespace PeopleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var mary = new Person { Name = "Mary" };
+            var harry = new Person { Name = "Harry" };
+            var jill = new Person { Name = "Jill" };
+
+            var baby1 = mary.ProcreateWith(harry);
+            baby1.Name = "Garry";
+
+            var baby2 = Person.Procreate(harry, jill);
+
+            var baby3 = harry * mary;
+
+            WriteLine($"{harry.Name} has {harry.Children.Count} children");
+            WriteLine($"{mary.Name} has {mary.Children.Count} children");
+            WriteLine($"{jill.Name} has {jill.Children.Count} children");
+
+            WriteLine(
+                format:"{0}'s first child name is \"{1}\"",
+                arg0: harry.Name,
+                arg1: harry.Children[0].Name
+            );
+        }
+    }
+}
